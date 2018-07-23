8527791b5ec4294d2b67d04cd9ab6f7ad88925d5
Aleksandr Ivanov <aivanov@hq.fix.ru>
2018-07-23 09:38:09 +0300

    return real plugins instead of a model

--- src/components/systems/plugins/PluginRepository.php
+++ src/components/systems/plugins/PluginRepository.php
@@ -3,6 +3,7 @@ namespace jeyroik\extas\components\systems\plugins;
 
 use jeyroik\extas\components\systems\Plugin;
 use jeyroik\extas\components\systems\repositories\RepositoryMongo;
+use jeyroik\extas\interfaces\systems\IPlugin;
 use jeyroik\extas\interfaces\systems\plugins\IPluginRepository;
 
 /**
@@ -15,4 +16,44 @@ class PluginRepository extends RepositoryMongo implements IPluginRepository
 {
     protected $itemClass = Plugin::class;
     protected $collectionName = 'extas__plugins';
+
+    /**
+     * @return IPlugin[]
+     */
+    public function all()
+    {
+        /**
+         * @var $pluginsModels IPlugin[]
+         */
+        $pluginsModels = parent::all();
+        $realPlugins = [];
+
+        if (!empty($pluginsModels)) {
+            foreach ($pluginsModels as $pluginModel) {
+                $className = $pluginModel->getClass();
+                $realPlugins[] = new $className();
+            }
+        }
+
+        return $realPlugins;
+    }
+
+    /**
+     * @return null|IPlugin
+     */
+    public function one()
+    {
+        /**
+         * @var $pluginModel IPlugin
+         */
+        $pluginModel = parent::one();
+
+        if ($pluginModel->getClass()) {
+            $className = $pluginModel->getClass();
+
+            return new $className();
+        }
+
+        return null;
+    }
 }
